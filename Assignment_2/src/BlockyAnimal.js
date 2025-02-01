cacd9840fa35ed12150e759363cd60acbe76e7b8
WilliamDemarest <wdemares@ucsc.edu>
2025-01-31 16:40:09 -0800

    basics and good animation and camera

--- Assignment_2/src/BlockyAnimal.js
+++ Assignment_2/src/BlockyAnimal.js
@@ -13,6 +13,7 @@ let a_Position;
 let u_FragColor;
 let u_ModelMatrix;
 let u_GlobalRotateMatrix;
+const fps_display = document.getElementById("fps");
 
 
 function setupWebGL(){
@@ -78,16 +79,24 @@ let g_selectedSize = 5;
 let g_globalAngle = [-20, 0, -10];
 let g_lastMouse = [200, 300];
 let g_headAngle = 20;
+let g_tailAngle = [0, 0, 0];
+let g_time = 0;
+let g_animation = false;
 // Set up actions for HTML UI elements
 function addActionsForHtmlUi(){
-  document.getElementById('clearButton').onclick = function() {g_shapesList = []; renderAllShapes(); };
+  document.getElementById('start').onclick = function() {g_animation = true; start_animation(); };
+  document.getElementById('stop').onclick = function() {g_animation = false; };
 
   document.getElementById('angleSlide').addEventListener('mousemove', function() {g_globalAngle[0] = parseInt(this.value); renderScene();} )
  
   document.getElementById('headAngle').addEventListener('mousemove', function() {g_headAngle = parseInt(this.value); renderScene();} );
+  document.getElementById('tailAngle1').addEventListener('mousemove', function() {g_tailAngle[0] = parseInt(this.value); renderScene();} );
+  document.getElementById('tailAngle2').addEventListener('mousemove', function() {g_tailAngle[1] = parseInt(this.value); renderScene();} );
+  document.getElementById('tailAngle3').addEventListener('mousemove', function() {g_tailAngle[2] = parseInt(this.value); renderScene();} );
 
   // document.getElementById('webgl').addEventListener('ondrag', function() {g_selectedSize = this.value; } );
-  document.getElementById('webgl').addEventListener('click', click );
+  document.getElementById('webgl').addEventListener('mousedown', click );
+  document.getElementById('webgl').addEventListener('mousemove', track );
 }
 
 function main() {
@@ -110,6 +119,12 @@ function main() {
 
 var g_shapesList = [];
 
+function track(ev) {
+  if(ev.buttons == 0){
+    g_lastMouse = [ev.clientX, ev.clientY];
+    //console.log(g_lastMouse);
+  }
+}
 
 function click(ev) {
   // Extract the event click and return it in WebGL coordinates
@@ -118,11 +133,10 @@ function click(ev) {
   const y = g_lastMouse[1] - ev.clientY; // y coordinate of a mouse pointer
 
   g_globalAngle[0] += x;
-  g_globalAngle[1] += y;
+  g_globalAngle[1] -= y;
   g_lastMouse = [ev.clientX, ev.clientY];
   // console.log(x + ", " + g_lastMouse[0] + " - " + ev.clientX);
   // console.log(g_globalAngle + ", " + y);
-
 
   renderScene();
 }
@@ -139,27 +153,39 @@ function convertCoordinatesEventToGl(ev){
   return([x, y]);
 }
 
+function start_animation(){
+  const start_time = performance.now();
+  tick();
+}
+
+function tick(){
+  g_time = performance.now();
+  renderScene();
+  if (g_animation) {
+    requestAnimationFrame(tick);
+  }
+}
+
 // Draw every shape that is supposed to be in the canvas
 function renderScene(){
-  // console.log(g_GlobalAngle);
-  // Clear <canvas>
+  const now = performance.now();
+
+  if (g_animation){
+    for (let i = 0; i < 3; i += 1){
+      g_tailAngle[i] = 10*Math.sin((g_time/100) - (i*45));
+    }
+  }
+
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   const globalRotMat = new Matrix4().rotate(g_globalAngle[0], 0, 1, 0);
   globalRotMat.rotate(g_globalAngle[1], 0, 0, 1);
   globalRotMat.rotate(g_globalAngle[2], 1, 0, 0);
-  // console.log(globalRotMat.elements);
   gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
 
-  /* var len = g_shapesList.length;
-  for(var i = 0; i < len; i++) {
-    g_shapesList[i].render();
-  } */
-  const rotateAnimal = [-10,-10]
   const colorAnimal = [.7, 0.7, 0.8, 1.0];
   const scaleAnimal = 0.5;
 
-  //const cubes = []
   const body2 = new Cube(); // base body
   body2.color = colorAnimal;
   body2.matrix.translate(-0.2, -0.255, -0.025, 1);
@@ -170,6 +196,7 @@ function renderScene(){
   body1.matrix.concat(body2.matrix);
   body1.color = colorAnimal;
   body1.matrix.translate(1.0, 0.0, 0.1, 1);
+  body1.matrix.rotate(g_tailAngle[0], 0, 0, 1);
   body1.matrix.scale(0.6, 0.8, 0.8, 1);
   body1.render();
 
@@ -177,6 +204,7 @@ function renderScene(){
   tailBody.matrix.concat(body1.matrix);
   tailBody.color = colorAnimal;
   tailBody.matrix.translate(1.0, 0.0, 0.1, 1);
+  tailBody.matrix.rotate(g_tailAngle[1], 0, 0, 1);
   tailBody.matrix.scale(0.6, 0.5, 0.8, 1);
   tailBody.render();
 
@@ -187,19 +215,18 @@ function renderScene(){
   tailLeft.offsetBottom = 0.5;
   tailLeft.matrix.translate(0.9, 0.25, 0.4, 1);
   tailLeft.matrix.rotate(80, 0, 1, 0);
-  tailLeft.matrix.rotate(90, 1, 0, 0);
+  tailLeft.matrix.rotate(90-g_tailAngle[2], 1, 0, 0);
   tailLeft.matrix.scale(0.8, 1.5, 0.25, 1);
   tailLeft.render();
 
-  const rightLeft = new Trapezoid(); // tail
+  const rightLeft = new Trapezoid(); 
   rightLeft.matrix.concat(tailBody.matrix);
   rightLeft.color = [0.6, 0.6, 0.7, 1.0];
-  rightLeft.offsetTop = 0.5;
-  rightLeft.offsetBottom = 1.2;
-  rightLeft.matrix.translate(2.4, 0.25, 0.35, 1);
-  rightLeft.matrix.rotate(90, 0, 1, 0);
-  rightLeft.matrix.rotate(90, 1, 0, 0);
-  rightLeft.matrix.rotate(170, 0, 0, 1);
+  rightLeft.offsetTop = -1.2;
+  rightLeft.offsetBottom = -0.5;
+  rightLeft.matrix.translate(0.9, 0.25, 0.6, 1);
+  rightLeft.matrix.rotate(100, 0, 1, 0);
+  rightLeft.matrix.rotate(90-g_tailAngle[2], 1, 0, 0);
   rightLeft.matrix.scale(0.8, 1.5, 0.25, 1);
   rightLeft.render();
 
@@ -213,10 +240,11 @@ function renderScene(){
   const leftFin = new Trapezoid();
   leftFin.matrix.concat(body3.matrix);
   leftFin.color = [0.6, 0.6, 0.7, 1.0];
-  leftFin.offsetTop = 0.5;
-  leftFin.offsetBottom = 1.2;
-  leftFin.matrix.translate(0.1, 0.15, -0.8, 1);
+  leftFin.offsetTop = -1.2;
+  leftFin.offsetBottom = -0.5;
+  leftFin.matrix.translate(0.1, 0.15, 0.0, 1);
   leftFin.matrix.rotate(90, 1, 0, 0);
+  leftFin.matrix.rotate(180-g_tailAngle[2], 0, 0, 1);
   leftFin.matrix.scale(0.6, 0.8, 0.15, 1);
   leftFin.render()
 
@@ -227,6 +255,7 @@ function renderScene(){
   rightFin.offsetBottom = 0.5;
   rightFin.matrix.translate(0.1, 0.15, 1, 1);
   rightFin.matrix.rotate(90, 1, 0, 0);
+  rightFin.matrix.rotate(g_tailAngle[2], 0, 0, 1);
   rightFin.matrix.scale(0.6, 0.8, 0.15, 1);
   rightFin.render()
 
@@ -251,4 +280,7 @@ function renderScene(){
   rightEye.matrix.translate(1.1, 0.5, 0.7, 1);
   rightEye.matrix.scale(-0.5*scaleAnimal, 0.5*scaleAnimal, 0.5*scaleAnimal, 1);
   rightEye.render();
+
+  const frame = performance.now() - now;
+  fps_display.textContent = "fps: " + 10000/frame;
 }
